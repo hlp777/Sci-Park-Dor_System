be1d5e98d80729ce2e80520fe9bdc69eef64395a
hlp777 <1367198389@qq.com>
2024-12-28 22:25:34 +0800

    对首页优化

--- spds-ui/src/views/index.vue
+++ spds-ui/src/views/index.vue
@@ -1,78 +1,119 @@
 <template>
-  <div class="app-container home">
-      <h1>欢迎使用科创园住宿服务系统</h1>
+  <div class="main">
+    <div class="bj">
+      <strong><h1 class="title"><span class="s">欢迎使用</span>科创园住宿服务系统</h1></strong>
+      <p class="p1">文明，廉洁，安全，舒适</p>
+    </div>
   </div>
 </template>
 
 <script setup name="Index">
-const version = ref('3.8.8')
+// const version = ref('3.8.8')
+// function goTarget(url) {
+//   window.open(url, '__blank')
+// }
 
-function goTarget(url) {
-  window.open(url, '__blank')
-}
+
 </script>
 
 <style scoped lang="scss">
-.home {
-  blockquote {
-    padding: 10px 20px;
-    margin: 0 0 20px;
-    font-size: 17.5px;
-    border-left: 5px solid #eee;
-  }
-  hr {
-    margin-top: 20px;
-    margin-bottom: 20px;
-    border: 0;
-    border-top: 1px solid #eee;
-  }
-  .col-item {
-    margin-bottom: 20px;
-  }
+// .home {
+//   blockquote {
+//     padding: 10px 20px;
+//     margin: 0 0 20px;
+//     font-size: 17.5px;
+//     border-left: 5px solid #eee;
+//   }
+//   hr {
+//     margin-top: 20px;
+//     margin-bottom: 20px;
+//     border: 0;
+//     border-top: 1px solid #eee;
+//   }
+//   .col-item {
+//     margin-bottom: 20px;
+//   }
 
-  ul {
-    padding: 0;
-    margin: 0;
-  }
+//   ul {
+//     padding: 0;
+//     margin: 0;
+//   }
 
-  font-family: "open sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
-  font-size: 13px;
-  color: #676a6c;
-  overflow-x: hidden;
+//   font-family: "open sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+//   font-size: 13px;
+//   color: #676a6c;
+//   overflow-x: hidden;
 
-  ul {
-    list-style-type: none;
-  }
+//   ul {
+//     list-style-type: none;
+//   }
 
-  h4 {
-    margin-top: 0px;
-  }
+//   h4 {
+//     margin-top: 0px;
+//   }
 
-  h2 {
-    margin-top: 10px;
-    font-size: 26px;
-    font-weight: 100;
-  }
+//   h2 {
+//     margin-top: 10px;
+//     font-size: 26px;
+//     font-weight: 100;
+//   }
 
-  p {
-    margin-top: 10px;
+//   p {
+//     margin-top: 10px;
 
-    b {
-      font-weight: 700;
-    }
-  }
+//     b {
+//       font-weight: 700;
+//     }
+//   }
 
-  .update-log {
-    ol {
-      display: block;
-      list-style-type: decimal;
-      margin-block-start: 1em;
-      margin-block-end: 1em;
-      margin-inline-start: 0;
-      margin-inline-end: 0;
-      padding-inline-start: 40px;
-    }
-  }
+//   .update-log {
+//     ol {
+//       display: block;
+//       list-style-type: decimal;
+//       margin-block-start: 1em;
+//       margin-block-end: 1em;
+//       margin-inline-start: 0;
+//       margin-inline-end: 0;
+//       padding-inline-start: 40px;
+//     }
+//   }
+// }
+
+.main {
+  height: 800px;
+  background-image: url("../assets/images/background1.jpg");
+  background-size: cover;
 }
+
+.bj {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.title {
+  font-size: 50px;
+  color: #fff;
+  text-align: center;
+  margin: 0;
+  padding: 100px 0 20px 0;
+}
+
+.s {
+  font-size: 65px;
+  color: rgb(39, 169, 245);
+  text-align: center;
+  margin: 0;
+}
+
+.p1 {
+  font-size: 30px;
+  color: #fff;
+  text-align: center;
+  margin: 0;
+}
+
+
 </style>
 
